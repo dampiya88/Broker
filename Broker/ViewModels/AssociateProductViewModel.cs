054c426ae3c2867edc9bb2b19301df79d15783a1
Don Mahathanthila <dampiya88@gmail.com>
2021-06-02 08:45:36 -0600

    ViewModel Added.Twitter-Bootstrap added.

---
+++ Broker/ViewModels/AssociateProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Broker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Broker.ViewModels
+{
+    public class AssociateProductViewModel
+    {
+        public IEnumerable<Product> Product { get; set; }
+        public IEnumerable<Associate> Associate { get; set; }
+
+    }
+}
